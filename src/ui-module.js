e1e63792cad0fea6149598f3cde0451f2ead5f76
Sasha <2010kvadratik@gmail.com>
2025-10-25 13:36:54 +0300

    add grid

--- src/ui-module.js
+++ src/ui-module.js
@@ -1,13 +1,13 @@
-const container = document.querySelector("#container");
+const folderContainer = document.querySelector("#folder-container");
 
 export function showFolders(folders) {
-  container.replaceChildren();
+  folderContainer.replaceChildren();
   for (let i = 0; i < folders.length; i++) {
     const folder = document.createElement("div");
     folder.textContent = folders[i].title;
     folder.setAttribute("id", folders[i].id);
     folder.className = "folder";
-    container.appendChild(folder);
+    folderContainer.appendChild(folder);
 
     folder.addEventListener("click", () => {
       localStorage.setItem("chosenFolder", folders[i].id);
