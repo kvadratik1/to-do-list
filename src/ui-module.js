0f3696495b8208359e3a9b0a4249cca8d89a2372
Sasha <2010kvadratik@gmail.com>
2025-10-25 13:24:38 +0300

    add folder style, folder on click event

--- src/ui-module.js
+++ src/ui-module.js
@@ -6,6 +6,11 @@ export function showFolders(folders) {
     const folder = document.createElement("div");
     folder.textContent = folders[i].title;
     folder.setAttribute("id", folders[i].id);
+    folder.className = "folder";
     container.appendChild(folder);
+
+    folder.addEventListener("click", () => {
+      localStorage.setItem("chosenFolder", folders[i].id);
+    });
   }
 }
